75bde63803c104a8678c6852ae28cbaae335976b
basazard <bagas123ft@gmail.com>
2021-01-29 10:36:57 +0700

    first commit

---
+++ src/java/login.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author basazard
+ */
+@ManagedBean (name = "value_login", eager = true)
+@RequestScoped
+public class login implements Serializable{
+
+    public String Username, Password;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    
+    
+    public String login() {
+        String Username = this.Username;
+        String Password = this.Password;
+        
+        if(Username.equals("basazard") && Password.equals("bagas123")){
+            return "berhasil";
+        }
+        else {
+            return "gagal";
+        }
+    }
+    
+}
